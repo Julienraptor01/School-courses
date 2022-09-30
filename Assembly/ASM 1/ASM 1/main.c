e76ddfac48b2b72940b0759233b83aad093120e3
Julienraptor01 <julienraptor01@gmail.com>
2022-09-30 09:31:36 +0200

    Finish assignement

--- Assembly/ASM 1/ASM 1/main.c
+++ Assembly/ASM 1/ASM 1/main.c
@@ -91,5 +91,70 @@ int main()
 		add i, 9
 	}
 	printf("\n9.\ni = %d\nj = %d\n", i, j);
+	i = -36;
+	j = 25;
+	_asm
+	{
+		mov eax, j
+		mov ebx, j
+		mov ecx, i
+		imul eax, ecx
+		imul ecx, 0x12
+		sub j, eax
+		add j, ebx
+		add j, ecx
+	}
+	printf("\n10.\ni = %d\nj = %d\n", i, j);
+	i = -36;
+	j = 25;
+	_asm
+	{
+		mov eax, j
+		mov ebx, j
+		mov ecx, i
+		imul eax, 18
+		mov j, eax
+		imul ebx, ecx
+		imul ebx, 178
+		add j, ecx
+		sub j, ebx
+	}
+	printf("\n11.\ni = %d\nj = %d\n", i, j);
+	i = -36;
+	j = 25;
+	_asm
+	{
+		mov eax, j
+		mov ebx, i
+		mov ecx, i
+		imul ebx, 5
+		imul ecx, eax
+		neg i
+		add i, 0x12
+		add i, ebx
+		sub i, ecx
+		sub i, 8
+	}
+	printf("\n12.\ni = %d\nj = %d\n", i, j);
+	i = -36;
+	j = 25;
+	_asm
+	{
+		mov eax, j
+		mov ebx, j
+		mov ecx, i
+		mov edx, i
+		imul eax, 0x1ab
+		imul ecx, 35
+		imul edx, edx
+		add i, eax
+		sub i, ecx
+		sub i, ebx
+		sub i, 8
+		sub i, 0x22
+		sub i, edx
+		sub i, 5
+	}
+	printf("\n13.\ni = %d\nj = %d\n", i, j);
 	return 0;
 }
