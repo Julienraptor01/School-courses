515c69bb2e810b3e88ae669ce778bd83958088a9
Julienraptor01 <julienraptor01@gmail.com>
2022-10-13 11:00:12 +0200

    small optimisation

--- C/Ordering numbers v2/main.c
+++ C/Ordering numbers v2/main.c
@@ -21,12 +21,12 @@ int main()
 		temp = number2;
 		number2 = number3;
 		number3 = temp;
-	}
-	if (number1 > number2)
-	{
-		temp = number1;
-		number1 = number2;
-		number2 = temp;
+		if (number1 > number2)
+		{
+			temp = number1;
+			number1 = number2;
+			number2 = temp;
+		}
 	}
 	printf("Les nombres dans l'ordre croissant sont %d, %d et %d\n", number1, number2, number3);
 	return 0;
