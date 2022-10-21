9ce229b38a41fdb169aaf3a5e04c7f9960f28989
Julienraptor01 <julienraptor01@gmail.com>
2022-10-21 13:56:13 +0200

    start ASM 3

--- Assembly/ASM 3/ASM 3/main.c
+++ Assembly/ASM 3/ASM 3/main.c
@@ -11,8 +11,45 @@ int main()
 	unsigned char ub = 5;
 	_asm
 	{
-		
+		movzx eax, ub
+		movsx ebx, b
+		movsx ecx, s
+		add eax, ebx
+		add eax, ecx
+		neg eax
+		mov i, eax
 	}
-	printf("\n1.\ni = %d\ns = %hd\nb = %hhd\nui = %u\nus = %hu\nub = %hhu\n", i, s, b, ui, us, ub);
+	printf("\n1.\ni = %d | s = %hd | b = %hhd\ni1 = %u | s1 = %hu | b1 = %hhu\n", i, s, b, ui, us, ub);
+	i = -3, s = 4, b = -2, ui = 8, us = 4, ub = 5;
+	_asm
+	{
+		movzx ebx, ub
+		neg ebx
+		movzx ecx, ui
+		add ebx, ecx
+		not ebx
+		movsx ecx, b
+		not ecx
+		mov eax, 0xf1
+		imul eax, ecx
+		or ebx, eax
+		mov s, bx
+	}
+	printf("\n2.\ni = %d | s = %hd | b = %hhd\ni1 = %u | s1 = %hu | b1 = %hhu\n", i, s, b, ui, us, ub);
+	i = -3, s = -1, b = -2, ui = 8, us = 4, ub = 5;
+	_asm
+	{
+		movsx eax, ui
+		movsx ebx, ub
+		imul eax, ebx
+		mov ebx, 2
+		movsx ecx, b
+		or ebx, ecx
+		neg ebx
+		cdq
+		idiv ebx
+		mov b, al
+	}
+	printf("\n3.\ni = %d | s = %hd | b = %hhd\ni1 = %u | s1 = %hu | b1 = %hhu\n", i, s, b, ui, us, ub);
 	return 0;
 }
