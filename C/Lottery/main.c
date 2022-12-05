c2687ddbcd5dd2d711ec1f10c0b26c20cb9a3dae
Julienraptor01 <julienraptor01@gmail.com>
2022-12-05 11:43:43 +0100

    Optimize mooore

--- C/Lottery/main.c
+++ C/Lottery/main.c
@@ -9,19 +9,14 @@ int randomNumber(int, int);
 int main()
 {
 	const int numberOfBalls = 7, smallestBallNumber = 1, biggestBallNumber = 45;
-	int lottery[8] = { 0 }, i;
+	int lottery[7] = { 0 }, i;
 	srand(time(NULL));
 	for (i = 0; i < numberOfBalls ; i++)
 	{
 		//generate a ball with randomNumber and verify it doesn't already exist with sentinelLinearSearch
-		if (sentinelLinearSearch(lottery, i, randomNumber(smallestBallNumber, biggestBallNumber)) == 1)
+		if (sentinelLinearSearch(lottery, i, randomNumber(smallestBallNumber, biggestBallNumber)) == 0)
 		{
-			//we assign the ball to its place in the array
-			lottery[i] = lottery[i + 1];
-		}
-		else
-		{
-			//we go back one step in the loop which will get us back to this step again
+			//we go back one step in the loop which will get us back to this step again if the number is not unique
 			i--;
 		}
 	}
@@ -36,24 +31,20 @@ int main()
 int sentinelLinearSearch(int *lottery, int actualBall, int generatedBall)
 {
 	//sentinel linear search
-	lottery[actualBall + 1] = generatedBall;
+	lottery[actualBall] = generatedBall;
 	int i;
 	//this is a for whitout a body because we only want to check if the generatedBall is already in the lottery array
 	for (i = 0; generatedBall != lottery[i]; i++) {}
+	//try to use a while instead of the for
 	/*
-	if (i == actualBall + 1)
+	while (generatedBall != lottery[i])
 	{
-		//good case
-		return 1;
-	}
-	else
-	{
-		//bad case were generatedBall is already in the lottery array
-		return 0;
+		i++;
 	}
 	*/
+	//it's actually slower
 	//this code here is a more optimized version which return 0 or 1 with the result of the comparison
-	return (i == actualBall + 1);
+	return (i == actualBall);
 }
 
 int randomNumber(int minRandom, int maxRandom)
