a730e016756147f2e5fed7c1925029b827923307
Julienraptor01 <julienraptor01@gmail.com>
2023-02-13 15:25:18 +0100

    finish

--- Arduino/Labo 2 RGB/Labo 2 RGB.ino
+++ Arduino/Labo 2 RGB/Labo 2 RGB.ino
@@ -1,47 +1,57 @@
+//color pin setup
 #define RED_PIN 10
 #define GREEN_PIN 11
 #define BLUE_PIN 9
 
+//create color type
 typedef struct {
 	int red;
 	int green;
 	int blue;
 } Color;
 
+//default colors (RGB)
 Color red = { 255, 0, 0 }, green = { 0, 255, 0 }, blue = { 0, 0, 255 }, magenta = { 255, 0, 255 }, yellow = { 255, 255, 0 }, cyan = { 0, 255, 255 }, white = { 255, 255, 255 }, black = { 0, 0, 0 };
-float dilution = 0;
-int waitTime = 2000;
+//init vars
+int dilution = 100, duration = 2000;
 
 void setup() {
-	// put your setup code here, to run once:
+	//pin setup
 	pinMode(RED_PIN, OUTPUT);
 	pinMode(GREEN_PIN, OUTPUT);
 	pinMode(BLUE_PIN, OUTPUT);
-	dilution = 0.5;
 }
 void loop() {
-	// put your main code here, to run repeatedly:
-	setColor(red, dilution, waitTime);
-	setColor(green, dilution, waitTime);
-	setColor(blue, dilution, waitTime);
-	setColor(white, dilution, waitTime);
-	setColor(magenta, dilution, waitTime);
-	setColor(yellow, dilution, waitTime);
-	setColor(cyan, dilution, waitTime);
+	//main part which cycle through all colors
+	setColor(red, dilution, duration);
+	setColor(green, dilution, duration);
+	setColor(blue, dilution, duration);
+	setColor(white, dilution, duration);
+	setColor(magenta, dilution, duration);
+	setColor(yellow, dilution, duration);
+	setColor(cyan, dilution, duration);
 }
 
+//function to set the non-diluted color
 void setPureColor(Color color) {
 	analogWrite(RED_PIN, color.red);
 	analogWrite(GREEN_PIN, color.green);
 	analogWrite(BLUE_PIN, color.blue);
 }
 
-void setColor(Color color, int dilution, int waitTime) {
-	while (waitTime > 0) {
+//function which dilute the color by toggling the color on and off over very short time
+void setColor(Color color, int dilution, int duration) {
+	//loop for duration
+	while (duration > 0) {
+		//set pure color
 		setPureColor(color);
-		delay(dilution);
+		//wait a bit
+		delayMicroseconds(dilution*10);
+		//now dilute it by setting it off
 		setPureColor(black);
-		delay(1-dilution);
-		waitTime--;
+		//wait the rest of the time
+		delayMicroseconds((100 - dilution)*10);
+		//decrease time passed
+		duration--;
 	}
 }
