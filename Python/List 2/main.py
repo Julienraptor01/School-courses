4b8e8497e9cddff49ee7769d00ac4dc8bd8f5354
Julienraptor01 <julienraptor01@gmail.com>
2023-02-17 13:36:50 +0100

    some multiline comment

--- Python/List 2/main.py
+++ Python/List 2/main.py
@@ -8,16 +8,18 @@ match input("Type the number of the exercise you want to run : "):
         print("Sum :", sum(int(i) for i in number), "Average :", sum(int(i) for i in number) / len(number))
     case "3":
         # Afficher une table de multiplication ainsi :
-        # 1   2   3   4   5   6   7   8   9  10
-        # 2   4   6   8  10  12  14  16  18  20
-        # 3   6   9  12  15  18  21  24  27  30
-        # 4   8  12  16  20  24  28  32  36  40
-        # 5  10  15  20  25  30  35  40  45  50
-        # 6  12  18  24  30  36  42  48  54  60
-        # 7  14  21  28  35  42  49  56  63  70
-        # 8  16  24  32  40  48  56  64  72  80
-        # 9  18  27  36  45  54  63  72  81  90
-        # 10 20  30  40  50  60  70  80  90 100
+        '''
+        1   2   3   4   5   6   7   8   9  10
+        2   4   6   8  10  12  14  16  18  20
+        3   6   9  12  15  18  21  24  27  30
+        4   8  12  16  20  24  28  32  36  40
+        5  10  15  20  25  30  35  40  45  50
+        6  12  18  24  30  36  42  48  54  60
+        7  14  21  28  35  42  49  56  63  70
+        8  16  24  32  40  48  56  64  72  80
+        9  18  27  36  45  54  63  72  81  90
+        10 20  30  40  50  60  70  80  90 100
+        '''
         # Pour afficher la table en alignant correctement les nombres en colonnes (4 caractères pour chaque colonne), utilisez la fonction format() décrite dans la section 1.2 des notes de cours.
         for i in range(1, 11):
             for j in range(1, 11):
@@ -25,15 +27,17 @@ match input("Type the number of the exercise you want to run : "):
             print()
     case "4":
         # Afficher un sapin en étoiles ainsi :
-        #     **
-        #    ****
-        #   ******
-        #  ********
-        # **********
-        #     **
-        #     **
-        #     **
-        #     **
+        '''
+            **
+           ****
+          ******
+         ********
+        **********
+            **
+            **
+            **
+            **
+        '''
         number = int(input("Enter the number of lines : "))
         for i in range(1, number + 1):
             print(" " * (number - i) + "*" * (2 * i))
