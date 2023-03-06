66f4992ab7b9a23a53ec749e8e66a57c72079706
Julienraptor01 <julienraptor01@gmail.com>
2023-03-06 16:08:49 +0100

    lab 4

--- Arduino/Lab 4 Ultrasonic Sensor and Servo/Lab 4 Ultrasonic Sensor and Servo.ino
+++ Arduino/Lab 4 Ultrasonic Sensor and Servo/Lab 4 Ultrasonic Sensor and Servo.ino
@@ -1,6 +1,9 @@
 //ping pin setup
 #define PING_PIN 7
 
+//servo pin setup
+#define SERVO_PIN 9
+
 //temperature
 #define TEMPERATURE 20
 
@@ -8,6 +11,8 @@
 #define SPEED_OF_SOUND 331.3 + 0.606 * TEMPERATURE
 
 void setup() {
+	//set the pin to output
+	pinMode(SERVO_PIN, OUTPUT);
 
 	//serial setup
 	Serial.begin(115200);
@@ -24,16 +29,30 @@ void loop() {
 
 	//read the pulse
 	pinMode(PING_PIN, INPUT);
-	long duration = pulseIn(PING_PIN, HIGH);
-	double distance = microsecondsToCentimeters(duration);
+	double distance = microsecondsToCentimeters(pulseIn(PING_PIN, HIGH));
 	Serial.print(distance);
 	Serial.println(" cm");
 
+	//move the servo to the right position if the distance is higher than 20 cm with a marge of 2 cm
+	if (distance > 21) {
+		pulseServo(1000);
+	} else if (distance > 19 && distance < 21) {
+		pulseServo(1500);
+	} else if (distance < 19) {
+		pulseServo(2000);
+	}
+
 	//wait a bit
-	delay(2000);
+	delay(100);
 }
 
 double microsecondsToCentimeters(long microseconds) {
 	return double(microseconds) * double(SPEED_OF_SOUND) / 20000.0;
 	//return double(microseconds) / 29 / 2;
+}
+
+void pulseServo(int pulseLength) {
+	digitalWrite(SERVO_PIN, HIGH);
+	delayMicroseconds(pulseLength);
+	digitalWrite(SERVO_PIN, LOW);
 }
