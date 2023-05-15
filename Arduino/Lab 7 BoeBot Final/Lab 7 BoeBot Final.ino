5271b9bf69ee6b97ab41908d6f2dea8149e18d07
Julienraptor01 <julienraptor01@gmail.com>
2023-05-15 15:37:06 +0200

    DUH

--- Arduino/Lab 7 BoeBot Final/Lab 7 BoeBot Final.ino
+++ Arduino/Lab 7 BoeBot Final/Lab 7 BoeBot Final.ino
@@ -1,3 +1,5 @@
+#include <math.h>
+
 //ping pin setup
 #define PING_PIN 7
 
@@ -18,20 +20,23 @@
 #define RIGHT_WHEEL_SERVO_CODER 2
 #define LEFT_WHEEL_SERVO_CODER 3
 
-//distance between the wheels
+//distances
 #define DISTANCE_BETWEEN_WHEELS 11.4
+#define DISTANCE_FROM_FRONT_TO_BACK 21
 
 //offsets to make the servo go straight
 #define RIGHT_WHEEL_SERVO_OFFSET 10
 #define LEFT_WHEEL_SERVO_OFFSET 0
 #define SENSOR_OFFSET 0
 
-int sensorServoLeft = 2400 + SENSOR_OFFSET, sensorServoMiddle = 1500 + SENSOR_OFFSET, sensorServoRight = 600 + SENSOR_OFFSET;
+int sensorServoLeft = 2500 + SENSOR_OFFSET, sensorServoMiddle = 1500 + SENSOR_OFFSET, sensorServoRight = 500 + SENSOR_OFFSET;
 int maxSensorPulseLength = (sensorServoLeft >= sensorServoRight) ? sensorServoLeft : sensorServoRight;
 int wheelLeftBackward = 1300 + LEFT_WHEEL_SERVO_OFFSET , wheelLeftStationary = 1500 + LEFT_WHEEL_SERVO_OFFSET , wheelLeftForward = 1700 + LEFT_WHEEL_SERVO_OFFSET ;
 int wheelRightBackward = 1700 + RIGHT_WHEEL_SERVO_OFFSET , wheelRightStationary = 1500 + RIGHT_WHEEL_SERVO_OFFSET , wheelRightForward = 1300 + RIGHT_WHEEL_SERVO_OFFSET ;
 int maxWheelPulseLength = (wheelRightBackward >= wheelLeftForward) ? wheelRightForward : wheelLeftBackward;
 volatile int wheelRightCounter, wheelLeftCounter;
+int step = 0;
+double distanceForward = 0, distanceRight[] = {0, 0};
 
 void setup()
 {
@@ -46,6 +51,15 @@ void setup()
 	//setup interrupt
 	attachInterrupt(digitalPinToInterrupt(RIGHT_WHEEL_SERVO_CODER), wheelRightCounterIncrease, CHANGE);
 	attachInterrupt(digitalPinToInterrupt(LEFT_WHEEL_SERVO_CODER), wheelLeftCounterIncrease, CHANGE);
+	for (int i = 0; i < 128; i++)
+	{
+		pulseSensorServo(sensorServoRight);
+		delay(2);
+	}
+	sendPingPulse();
+	distanceRight[0] = readPingPulse();
+	Serial.print(distanceRight[0]);
+	Serial.print(" cm\t");
 }
   
 void loop()
@@ -60,11 +74,75 @@ void loop()
 	rotateRightOnMiddle(90);
 	delay(1000000);
 	*/
+	/*
 	move(1, 1450, 1550, false);
-	delay(1000000);
+	delay(1);
+	*/
+	if (step == 0)
+	{
+		moveForward(1);
+		sendPingPulse();
+		distanceRight[1] = readPingPulse();
+		Serial.print(distanceRight[1]);
+		Serial.println(" cm");
+		if (distanceRight[1] - distanceRight[0] > DISTANCE_BETWEEN_WHEELS * 0.6)
+		{
+			step = 1;
+		}
+		else
+		{
+			distanceRight[0] = distanceRight[1];
+			Serial.print(distanceRight[0]);
+			Serial.print(" cm\t");
+		}
+	}
+	else if (step == 1)
+	{
+		moveForward(1);
+		distanceForward += 1;
+		sendPingPulse();
+		distanceRight[0] = readPingPulse();
+		Serial.print(distanceRight[0]);
+		Serial.print(" cm\t");
+		Serial.print(distanceRight[1]);
+		Serial.print(" cm\t");
+		Serial.print(distanceForward);
+		Serial.println(" cm");
+		if (distanceRight[1] - distanceRight[0] > DISTANCE_BETWEEN_WHEELS * 0.6)
+		{
+			step = 2;
+		}
+		else
+		{
+			distanceRight[0] = distanceRight[1];
+		}
+	}
+	else if (step == 2)
+	{
+		moveForward(7);
+		distanceForward += 7;
+		step = 3;
+		Serial.print(distanceForward);
+		Serial.println(" cm");
+	}
+	else if (step == 3)
+	{
+		rotateLeftOnMiddle(45);
+		step = 4;
+	}
+	else if (step == 4)
+	{
+		moveBackward(sqrt(pow(distanceRight[1] - distanceRight[0], 2) + pow(distanceForward / 2, 2)) * 0.95);
+		step = 5;
+	}
+	else if (step == 5)
+	{
+		rotateRightOnMiddle(45);
+		step = 6;
+	}
 }
 
-void rotateLeftOnLeft(int angle)
+void rotateForwardLeftOnLeft(int angle)
 {
 	move(angle * DISTANCE_BETWEEN_WHEELS * 2 * M_PI / 360, wheelRightForward, wheelLeftStationary, true);
 }
@@ -79,7 +157,7 @@ void rotateRightOnMiddle(int angle)
 	move((angle / 2) * DISTANCE_BETWEEN_WHEELS * 2 * M_PI / 360, wheelRightBackward, wheelLeftForward, true);
 }
 
-void rotateRightOnRight(int angle)
+void rotateForwardRightOnRight(int angle)
 {
 	move(angle * DISTANCE_BETWEEN_WHEELS * 2 * M_PI / 360, wheelRightStationary, wheelLeftForward, true);
 }
@@ -87,6 +165,11 @@ void rotateRightOnRight(int angle)
 void moveForward(int distance)
 {
 	move(distance, wheelRightForward, wheelLeftForward, true);
+}
+
+void moveBackward(int distance)
+{
+	move(distance, wheelRightBackward, wheelLeftBackward, true);
 }
 
 void move(int value, int wheelLeft, int wheelRight, bool isDistance)
